b975043d1bdf239a9abb29c10678cb13d6bacb01
Matthew Kozlowski <mateusz@foreflight.com>
2022-06-08 15:11:45 -0500

    Improved integration testing framework

--- src/test/java/com/foreflight/apphelper/AppHelperApplicationTests.java
+++ src/test/java/com/foreflight/apphelper/AppHelperApplicationTests.java
@@ -1,10 +1,11 @@
 package com.foreflight.apphelper;
 
-
+import com.foreflight.apphelper.domain.MenuChoice;
 import com.foreflight.apphelper.repository.MenuChoiceRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -12,34 +13,36 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@Testcontainers
-//@DataJpaTest
-//@ActiveProfiles("test")
-//@DirtiesContext
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class AppHelperApplicationTests {
 
     @Autowired
     private MenuChoiceRepository menuChoiceRepository;
 
-
-    @Container
-    public static PostgreSQLContainer<?> postgresDB = new PostgreSQLContainer<>
-            ("postgres")
-            .withDatabaseName("postgres")
-            .withUsername("user")
-            .withPassword("pass");
-
-
-    @DynamicPropertySource
-    public static void properties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgresDB::getJdbcUrl);
-        registry.add("spring.datasource.username", postgresDB::getUsername);
-        registry.add("spring.datasource.password", postgresDB::getPassword);
+    @Test
+    void contextLoads() {
     }
 
     @Test
-    void contextLoads() {
+    void testDb(){
+        MenuChoice choice = new MenuChoice("test", null, null);
+
+        menuChoiceRepository.save(choice);
+
+        for(MenuChoice foundChoice : menuChoiceRepository.findAll()){
+            System.out.println(foundChoice.getName());
+        }
+    }
+
+    @Test
+    void testDb2(){
+        MenuChoice choice = new MenuChoice("test2", null, null);
+
+        menuChoiceRepository.save(choice);
+
+        for(MenuChoice foundChoice : menuChoiceRepository.findAll()){
+            System.out.println(foundChoice.getName());
+        }
     }
 
 
