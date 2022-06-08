8c54bddd43d615ba1ce69b8bd15bcb8c7ba7c3a3
Matthew Kozlowski <mateusz@foreflight.com>
2022-06-08 14:53:41 -0500

    Added support for integration tests

--- src/test/java/com/foreflight/apphelper/AppHelperApplicationTests.java
+++ src/test/java/com/foreflight/apphelper/AppHelperApplicationTests.java
@@ -1,13 +1,46 @@
 package com.foreflight.apphelper;
 
+
+import com.foreflight.apphelper.repository.MenuChoiceRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
+//@DataJpaTest
+//@ActiveProfiles("test")
+//@DirtiesContext
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class AppHelperApplicationTests {
+
+    @Autowired
+    private MenuChoiceRepository menuChoiceRepository;
+
+
+    @Container
+    public static PostgreSQLContainer<?> postgresDB = new PostgreSQLContainer<>
+            ("postgres")
+            .withDatabaseName("postgres")
+            .withUsername("user")
+            .withPassword("pass");
+
+
+    @DynamicPropertySource
+    public static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresDB::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresDB::getUsername);
+        registry.add("spring.datasource.password", postgresDB::getPassword);
+    }
 
     @Test
     void contextLoads() {
     }
+
 
 }
