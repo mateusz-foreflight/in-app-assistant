75d87dfe279756fa19991f981a715e527b177bd6
Matthew Kozlowski <mateusz@foreflight.com>
2022-06-08 17:46:42 -0500

    Added some simple tests for a couple menuchoice endpoints

--- src/test/java/com/foreflight/apphelper/AppHelperApplicationTests.java
+++ src/test/java/com/foreflight/apphelper/AppHelperApplicationTests.java
@@ -6,22 +6,28 @@ import com.foreflight.apphelper.repository.MenuChoiceRepository;
 import com.foreflight.apphelper.repository.ResourceRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
 class AppHelperApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Autowired
     private MenuChoiceRepository menuChoiceRepository;
@@ -29,6 +35,8 @@ class AppHelperApplicationTests {
     @Autowired
     private ResourceRepository resourceRepository;
 
+
+    
     @Test
     void contextLoads() {
     }
@@ -42,13 +50,17 @@ class AppHelperApplicationTests {
         menuChoiceRepository.save(choice1);
         menuChoiceRepository.save(choice2);
 
+        for(MenuChoice mc : menuChoiceRepository.findAll()){
+            System.out.println(mc.getName());
+        }
+
         List<MenuChoice> foundChoices = menuChoiceRepository.findAll();
         assertThat(foundChoices).contains(choice1, choice2);
     }
 
     @Test
     @Transactional
-    void resourceRepositoryWorks(){
+    void resourceRepositoryWorks() throws Exception {
         Resource resource1 = new Resource("test", "testLink");
         Resource resource2 = new Resource("test2", "testLink2");
 
@@ -57,6 +69,45 @@ class AppHelperApplicationTests {
 
         List<Resource> foundResources = resourceRepository.findAll();
         assertThat(foundResources).contains(resource1, resource2);
+
+        mockMvc.perform(get("/api/v1/resources"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[?(@.name == \"test\" && @.link == \"testLink\")]").exists());
+
+
+    }
+
+    @Test
+    @Transactional
+    void getAllMenuChoices() throws Exception{
+        List<Resource> resources = new ArrayList<>();
+        resources.add(new Resource("res1", "link"));
+        resources.add(new Resource("res2", "link"));
+        resourceRepository.saveAll(resources);
+
+        List<MenuChoice> choices = new ArrayList<>();
+        choices.add(new MenuChoice("test1", null, new ArrayList<>()));
+        choices.add(new MenuChoice("test2", choices.get(0), new ArrayList<>()));
+        choices.add(new MenuChoice("test3", choices.get(1), resources));
+        menuChoiceRepository.saveAll(choices);
+
+        mockMvc.perform(get("/api/v1/menuchoices"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[?(@.name == \"test1\" && @.parent == null && @.resources == [])]").exists())
+                .andExpect(jsonPath("$.[?(@.name == \"test2\" && @.parent.name == \"test1\"  && @.resources == [])]").exists())
+                .andExpect(jsonPath("$.[?(@.name == \"test3\" && @.parent.name == \"test2\" && [\"res2\", \"res1\"] subsetof @.resources[*].name && @.resources.length() == 2)]").exists());
+    }
+
+    @Test
+    @Transactional
+    void getMenuChoiceById() throws Exception{
+        MenuChoice choice1 = new MenuChoice("test1", null, null);
+
+        Long id = menuChoiceRepository.save(choice1).getId();
+
+        mockMvc.perform(get("/api/v1/menuchoices/" + id))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath(String.format("$.[?(@.name == \"test1\" && @.id == %d)]", id)).exists());
     }
 
 }
