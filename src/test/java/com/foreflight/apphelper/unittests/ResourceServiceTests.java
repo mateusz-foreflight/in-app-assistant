4e5f3bf61ba572d81c96eb28c612ecf53159628d
Matthew Kozlowski <mateusz@foreflight.com>
2022-06-09 15:23:18 -0500

    Added resource service unit tests

---
+++ src/test/java/com/foreflight/apphelper/unittests/ResourceServiceTests.java
@@ -0,0 +1,181 @@
+package com.foreflight.apphelper.unittests;
+
+import com.foreflight.apphelper.domain.MenuChoice;
+import com.foreflight.apphelper.domain.Resource;
+import com.foreflight.apphelper.domain.ResourceDTO;
+import com.foreflight.apphelper.repository.ResourceRepository;
+import com.foreflight.apphelper.service.ResourceService;
+import com.foreflight.apphelper.unittests.mockprofiles.MockProfile1;
+import com.foreflight.apphelper.unittests.mockprofiles.MockProfile2;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class ResourceServiceTests {
+
+    @Mock
+    private ResourceRepository resourceRepository;
+
+    private ResourceService resourceService;
+
+    @BeforeEach
+    void init(){
+        resourceRepository = mock(ResourceRepository.class);
+
+        resourceService = new ResourceService(resourceRepository);
+    }
+
+    @Test
+    void getAllResources_works(){
+        // Given
+        MockProfile2 mockProfile = new MockProfile2(resourceRepository);
+
+        // When
+        List<Resource> actual = resourceService.getAllResources();
+
+        // Then
+        List<Resource> expected = mockProfile.resources;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void getResourceById_withValidId(){
+        // Given
+        MockProfile2 mockProfile = new MockProfile2(resourceRepository);
+
+        // When
+        Optional<Resource> actual = resourceService.getResourceById(1L);
+
+        // Then
+        assertThat(actual.isPresent()).isTrue();
+        assertThat(actual.get()).isEqualTo(mockProfile.resource1);
+    }
+
+    @Test
+    void getResourceById_withInvalidId(){
+        // Given
+        MockProfile2 mockProfile = new MockProfile2(resourceRepository);
+
+        // When
+        Optional<Resource> actual = resourceService.getResourceById(15L);
+
+        // Then
+        assertThat(actual.isPresent()).isFalse();
+    }
+
+    @Test
+    void getResourceByName_withValidName(){
+        // Given
+        MockProfile2 mockProfile = new MockProfile2(resourceRepository);
+
+        // When
+        Optional<Resource> actual = resourceService.getResourceByName("resource1");
+
+        // Then
+        assertThat(actual.isPresent()).isTrue();
+        assertThat(actual.get()).isEqualTo(mockProfile.resource1);
+    }
+
+    @Test
+    void getResourceByName_withInvalidName(){
+        // Given
+        MockProfile2 mockProfile = new MockProfile2(resourceRepository);
+
+        // When
+        Optional<Resource> actual = resourceService.getResourceByName("doesNotExist");
+
+        // Then
+        assertThat(actual.isPresent()).isFalse();
+    }
+
+    @Test
+    void addResource_withValidResource(){
+        // Given
+        MockProfile2 mockProfile = new MockProfile2(resourceRepository);
+        ResourceDTO dto = new ResourceDTO("new", "newLink");
+
+        // When
+        Resource actual = resourceService.addResource(dto);
+        actual.setId(5L);
+
+        // Then
+        Resource expected = new Resource("new", "newLink");
+        expected.setId(5L);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void addResource_withInvalidResource_withInvalidName(){
+        // Given
+        MockProfile2 mockProfile = new MockProfile2(resourceRepository);
+        ResourceDTO dto = new ResourceDTO(null, "newLink");
+
+        // When, Then
+        assertThatExceptionOfType(IllegalStateException.class)
+                .isThrownBy(() -> resourceService.addResource(dto))
+                .withMessageContaining("name")
+                .withMessageContaining("null");
+    }
+
+    @Test
+    void updateResource_withPresentId(){
+        // Given
+        MockProfile2 mockProfile = new MockProfile2(resourceRepository);
+        ResourceDTO dto = new ResourceDTO("new", "newLink");
+
+        // When
+        Resource actual = resourceService.updateResource(dto, 1L);
+
+        // Then
+        Resource expected = new Resource("new", "newLink");
+        expected.setId(1L);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void updateResource_withNotPresentId(){
+        // Given
+        MockProfile2 mockProfile = new MockProfile2(resourceRepository);
+        ResourceDTO dto = new ResourceDTO("new", "newLink");
+
+        // When
+        Resource actual = resourceService.updateResource(dto, 15L);
+
+        // Then
+        Resource expected = new Resource("new", "newLink");
+        expected.setId(15L);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void deleteResource_withValidId(){
+        //Given
+        MockProfile2 mockProfile = new MockProfile2(resourceRepository);
+
+        // When
+        resourceService.deleteResource(1L);
+
+        // Then
+        verify(resourceRepository).deleteById(1L);
+    }
+
+    @Test
+    void deleteChoice_withInvalidId(){
+        //Given
+        MockProfile2 mockProfile = new MockProfile2(resourceRepository);
+
+        // When, Then
+        assertThatExceptionOfType(IllegalStateException.class)
+                .isThrownBy(() -> resourceService.deleteResource(15L))
+                .withMessageContaining("id")
+                .withMessageContaining(String.valueOf(15));
+    }
+}
---
+++ src/test/java/com/foreflight/apphelper/unittests/mockprofiles/MockProfile2.java
@@ -0,0 +1,61 @@
+package com.foreflight.apphelper.unittests.mockprofiles;
+
+import com.foreflight.apphelper.domain.MenuChoice;
+import com.foreflight.apphelper.domain.Resource;
+import com.foreflight.apphelper.repository.MenuChoiceRepository;
+import com.foreflight.apphelper.repository.ResourceRepository;
+import com.foreflight.apphelper.service.ResourceService;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+
+// Mocks a resource repository with the following Resource objects in it:
+//      id: 1, name: resource1, link: link1
+//      id: 2, name: resource2, link: null
+public class MockProfile2 {
+    public Resource resource1;
+    public Resource resource2;
+    public List<Resource> resources;
+
+
+    public MockProfile2(ResourceRepository resourceRepository){
+        // SET UP DATA
+        // Set up resources
+        this.resource1 = new Resource("resource1", "link2");
+        this.resource1.setId(1L);
+        this.resource2 = new Resource("resource2", null);
+        this.resource2.setId(2L);
+        this.resources = Arrays.asList(resource1, resource2);
+
+
+        // SET UP METHOD MOCKS
+        // resourceRepository - save, findAll, findResourceById, findResourceByName, existsById
+        when(resourceRepository.save(Mockito.any(Resource.class))).then(returnsFirstArg());
+
+        when(resourceRepository.findAll()).thenReturn(resources);
+
+        // Take advantage of overriding stubbing as described here:
+        // https://stackoverflow.com/questions/34171082/how-to-handle-any-other-value-with-mockito
+        when(resourceRepository.findResourceById(anyLong())).thenReturn(Optional.empty());
+        doReturn(Optional.of(resource1)).when(resourceRepository).findResourceById(1L);
+        doReturn(Optional.of(resource1)).when(resourceRepository).findResourceById(2L);
+
+        when(resourceRepository.findResourceByName(anyString())).thenReturn(Optional.empty());
+        doReturn(Optional.of(resource1)).when(resourceRepository).findResourceByName("resource1");
+        doReturn(Optional.of(resource2)).when(resourceRepository).findResourceByName("resource2");
+
+        when(resourceRepository.existsById(anyLong())).thenReturn(false);
+        doReturn(true).when(resourceRepository).existsById(1L);
+        doReturn(true).when(resourceRepository).existsById(2L);
+
+    }
+}
